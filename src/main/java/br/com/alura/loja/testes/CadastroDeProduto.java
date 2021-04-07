a1d5b286bfbe3db76ca0a632a747849ed7842e58
João Pedro <joao.pedro42@outlook.com.br>
2021-04-07 12:04:18 -0300

    Subindo projeto já com o persistence configurado e a funcionalidade de cadastrar produtos

---
+++ src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -0,0 +1,27 @@
+package br.com.alura.loja.testes;
+
+import br.com.alura.loja.dao.ProdutoDao;
+import br.com.alura.loja.modelo.Produto;
+import br.com.alura.loja.util.JPAUtil;
+
+import javax.persistence.EntityManager;
+import java.math.BigDecimal;
+
+public class CadastroDeProduto {
+
+    public static void main(String[] args) {
+        Produto celular = new Produto();
+        celular.setNome("Xiaomi Redmi");
+        celular.setDescricao("Muito legal");
+        celular.setPreco(new BigDecimal("800"));
+
+        EntityManager em = JPAUtil.getEntityManager();
+
+        ProdutoDao dao = new ProdutoDao(em);
+
+        em.getTransaction().begin();
+        dao.cadastrar(celular);
+        em.getTransaction().commit();
+        em.close();
+    }
+}
