d544eeb0dc8de69fd804564efbc98969ceb8ba0a
João Pedro <joao.pedro42@outlook.com.br>
2021-04-08 19:14:30 -0300

    Transformando enum Categoria em entidade e configurando mapeamento entre Categoria e Produto

--- src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -1,5 +1,6 @@
 package br.com.alura.loja.testes;
 
+import br.com.alura.loja.dao.CategoriaDao;
 import br.com.alura.loja.dao.ProdutoDao;
 import br.com.alura.loja.modelo.Categoria;
 import br.com.alura.loja.modelo.Produto;
@@ -11,16 +12,20 @@ import java.math.BigDecimal;
 public class CadastroDeProduto {
 
     public static void main(String[] args) {
+        Categoria celulares = new Categoria("celulares");
         Produto celular = new Produto("Xiaomi Redmi",
                 "Muito legal",
-                new BigDecimal("800"), Categoria.CELULARES);
+                new BigDecimal("800"), celulares);
 
         EntityManager em = JPAUtil.getEntityManager();
-
-        ProdutoDao dao = new ProdutoDao(em);
+        CategoriaDao categoriaDao = new CategoriaDao(em);
+        ProdutoDao produtoDao = new ProdutoDao(em);
 
         em.getTransaction().begin();
-        dao.cadastrar(celular);
+
+        categoriaDao.cadastrar(celulares);
+        produtoDao.cadastrar(celular);
+
         em.getTransaction().commit();
         em.close();
     }
